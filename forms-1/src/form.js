480f2541c555af5d7858046f23e7d41f04615e10
Alex <alexpark@pursuit.org>
2019-01-09 17:07:51 -0500

    forms1-2NoCSS

--- forms-1/src/form.js
+++ forms-1/src/form.js
@@ -13,14 +13,46 @@ class Form extends Component {
             diet: '',
             reason: '',
             formCompleted: false,
-            formSubmitted: false
+            formSubmitted: false,
+            formConfirmed: false,
+            underWater: '',
+            marital: '',
+            stressReaction: '',
+            claustrophobic: '',
+            familyHistory: {
+                cancer: false,
+                heartDisease: false,
+                diabetes: false
+            },
+            family: {
+                siblings: false,
+                siblingCount: 0,
+                parents: false,
+                parentCount: 0,
+                gParents: false,
+                gParentCount: 0
+            },
+            education: {
+                diploma: false,
+                associates: false,
+                bachelors: false,
+                masters: false,
+                phd: false,
+                other: ''
+            }
         }
     }
 
-    handleSubmit = () => {
-        this.setState({
-            formSubmitted: true
-        })
+    handleButton = () => {
+        if (event.target.id === 'confirm') {
+            this.setState({
+                formConfirmed: true
+            })
+        } else {
+            this.setState({
+                formSubmitted: true
+            })
+        }
     }
     
     isFormComplete = () => {
@@ -30,19 +62,62 @@ class Form extends Component {
                 formCompleted: true
             })
         }
-        console.log(this.state)
-    } 
 
-    handleFormChanges = (event) => {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
+    };
+
+    handleTextChanges = (event) => {
+        if (event.target.id) {
+            let newState = this.state[event.target.name];
+            newState[event.target.id] = event.target.value
+            this.setState({
+                [event.target.name]: newState
+            })
+        } else {
+            this.setState({
+                [event.target.name]: event.target.value
+            })
+        }
         this.isFormComplete();
     };
 
+    handleRadioChanges = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    };
+
+    handleCheckboxChanges = (event) => {
+        if (event.target.id) {
+            let newState = this.state[event.target.name];
+            newState[event.target.id] = event.target.checked
+            this.setState({
+                [event.target.name]: newState
+            })
+        } else {
+            this.setState({
+                [event.target.name]: event.target.checked
+            })
+        }
+    };
+
+    handleSelectChanges = (event) => {
+        if (event.target.id) {
+            let newState = this.state[event.target.name];
+            newState[event.target.id] = event.target.value
+            this.setState({
+                [event.target.name]: newState
+            })
+        } else {
+            this.setState({
+                [event.target.name]: event.target.value
+            })
+        }
+        this.isFormComplete();
+    };
+   
 
     render () {
-        const { name, month, day, year, country, diet, reason, formCompleted, formSubmitted } = this.state;
+        const { name, month, day, year, country, diet, reason, formCompleted, formSubmitted, underWater, marital, stressReaction, claustrophobic, family, familyHistory, education, formConfirmed } = this.state;
         let allDays = [];
         let allMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
         let allYears = [];
@@ -60,10 +135,53 @@ class Form extends Component {
                     <div className='confirmation'>
                     <h3>Please confirm the information.</h3>
                     <p>Name: { this.state.name }</p>
-                    <p>DOB: { this.state.month } { this.state.day } { this.state.year }</p>
+                    <p>DOB: { this.state.month }, { this.state.day }, { this.state.year }</p>
                     <p>Country: { this.state.country }</p>
                     <p>Dietary Preference: { this.state.diet }</p>
                     <p>Reason: { this.state.reason }</p>
+                    </div>
+                )
+            }
+        }
+
+        const renderFamilySelect = (familyMember, value, id) => {
+            if(familyMember && (id === 'siblingCount')) {
+                return (
+                   <div className='siblingSelect'>
+                    <p>How many?</p>
+                    <select name='family' value={ value } id={ id } onChange={ this.handleSelectChanges }>
+                        <option value=''>0</option>
+                        <option value='1'>1</option>
+                        <option value='2'>2</option>
+                        <option value='3'>3</option>
+                        <option value='4'>4</option>
+                        <option value='5'>5</option>
+                        <option value='6+'>6+</option>
+                    </select>
+                    </div> 
+                )
+            } else if (familyMember && (id === 'parentCount')) {
+                return (
+                    <div className='parentSelect'>
+                        <p>How many?</p>
+                        <select name='family' value={ value } id={ id } onChange={ this.handleSelectChanges }>
+                            <option value=''>0</option>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                        </select>
+                    </div>
+                )
+            } else if (familyMember && (id === 'gParentCount')) {
+                return (
+                    <div className='gParentSelect'>
+                        <p>How many?</p>
+                        <select name='family' value={ value } id={ id } onChange={ this.handleSelectChanges }>
+                            <option value=''>0</option>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                            <option value='3'>3</option>
+                            <option value='4'>4</option>
+                        </select>
                     </div>
                 )
             }
@@ -87,24 +205,24 @@ class Form extends Component {
     
                     <div className='form'>
                       
-                      <form onChange={ this.handleFormChanges }>  
+                      <form>  
                         <div className='name'>
-                            What is your name? <input type='text' placeholder='Name' name='name' value={ name } id='name'></input>
+                            What is your name? <input onChange={ this.handleTextChanges } type='text' placeholder='Name' name='name' value={ name }></input>
                         </div>
                         
                         <div className='dob'>    
                             What is your date of birth? 
-                            <select name='month' value={ month }>
+                            <select  onChange={ this.handleSelectChanges } name='month' value={ month }>
                                 <option value='month'>Month</option>
                                 { createMonths(allMonths) }
                             </select>
     
-                            <select name='day' value={ day }>
+                            <select  onChange={ this.handleSelectChanges } name='day' value={ day }>
                                 <option value='day'>Day</option>
                                 { createDateOptions(1, 31, allDays) }
                             </select>    
     
-                            <select name='year' value={ year }>
+                            <select  onChange={ this.handleSelectChanges } name='year' value={ year }>
                                 <option value='year'>Year</option>
                                 { createDateOptions(1900, 2019, allYears) }
                             </select>
@@ -112,7 +230,7 @@ class Form extends Component {
     
                         <div className='country'>
                         What is your country of origin?
-                            <select name='country' value={ country }>
+                            <select  onChange={ this.handleSelectChanges } name='country' value={ country }>
                                 <option value='country'>Country</option>
                                 { createCountries(countries) }
                             </select>
@@ -120,23 +238,129 @@ class Form extends Component {
     
                         <div className='diet'>
                         What is your dietary preference?
-                            <select name='diet' value={ diet }>
-                                <option value='diet'>Dietary Preference</option>
-                                <option value='omnivore'>Omnivore</option>
-                                <option value='vegetarian'>Vegetarian</option>
-                                <option value='vegan'>Vegan</option>
-                                <option value='carnivore'>Carnivore</option>
+                            <select  onChange={ this.handleSelectChanges } name='diet' value={ diet }>
+                                <option value='Diet'>Dietary Preference</option>
+                                <option value='Omnivore'>Omnivore</option>
+                                <option value='Vegetarian'>Vegetarian</option>
+                                <option value='Vegan'>Vegan</option>
+                                <option value='Carnivore'>Carnivore</option>
                             </select>
+                        </div>
+
+                        <div className='underwater'>
+                            Can you breathe underwater longer than 1 minute?
+                            <div>
+                                Yes<input value='Yes' name='underWater' type='radio' checked={ underWater === 'Yes' } onChange={ this.handleRadioChanges }/>
+                            </div>
+                            <div>
+                                No<input value='No' name='underWater' type='radio' checked={ underWater === 'No' } onChange={ this.handleRadioChanges }/>
+                            </div>
+                            <div>
+                                I don't know<input value='I do not know' name='underWater' type='radio' checked={ underWater === 'idk' } onChange={ this.handleRadioChanges }/>
+                            </div>
+                        </div>
+
+                        <div className='marital'>
+                            What is your marital status?
+                            <div>
+                                Married<input value='Married' name='marital' type='radio' checked={ marital === 'Married' } onChange={ this.handleRadioChanges }/>
+                            </div>
+                            <div>
+                                Single<input value='Single' name='marital' type='radio' checked={ marital === 'Single' } onChange={ this.handleRadioChanges }/>
+                            </div>
+                        </div>
+
+                        <div className='stressReaction'>
+                            When you are in a stressful or difficult situation, how do you most frequently react?
+                            <div>
+                                Determination: I continue to confront the situation.<input value='Determined' name='stressReaction' type='radio' checked={ stressReaction === 'Determined' } onChange={ this.handleRadioChanges }/>
+                            </div>
+                            <div>
+                                Defeat: I stop confronting the situation.<input value='Defeated' name='stressReaction' type='radio' checked={ stressReaction === 'Defeated' } onChange={ this.handleRadioChanges }/>
+                            </div>
+                            <div>
+                                Anger: I become upset at the situation.<input value='Angered' name='stressReaction' type='radio' checked={ stressReaction === 'Angered' } onChange={ this.handleRadioChanges }/>
+                            </div>
+                            <div>
+                                Resourcefulness: I seek help to confront the situation.<input value='Resourcefulness' name='stressReaction' type='radio' checked={ stressReaction === 'Resourcefulness' } onChange={ this.handleRadioChanges }/>    
+                            </div>
+                            <div>
+                                Flatulence: I spend 2 hours farting away stress.<input value='Flatulence' name='stressReaction' type='radio' checked={ stressReaction === 'Flatulence' } onChange={ this.handleRadioChanges }/>
+                            </div>
+                        </div>
+
+                        <div className='claustrophobic'>
+                            Are you claustrophobic?
+                            <div>
+                                Yes<input value='Yes' name='claustrophobic' type='radio' checked={ claustrophobic === 'Yes' } onChange={ this.handleRadioChanges }/>
+                            </div>
+                            <div>
+                                No<input value='No' name='claustrophobic' type='radio' checked={ claustrophobic === 'No' } onChange={ this.handleRadioChanges }/>
+                            </div>
+                            <div>
+                                I don't know<input value='I do not know' name='claustrophobic' type='radio' checked={ claustrophobic === 'I do not know' } onChange={ this.handleRadioChanges }/>
+                            </div>
+                        </div>
+
+                        <div className='familyHistory'>
+                            Does your family have a history of (check all that apply):
+                            <div>
+                                Cancer<input id='cancer' name='familyHistory' type='checkbox' checked={ familyHistory.cancer } onChange={ this.handleCheckboxChanges } />
+                            </div>
+                            <div>
+                                Heart Disease<input id='heartDisease' name='familyHistory' type='checkbox' checked={ familyHistory.heartDisease } onChange={ this.handleCheckboxChanges } />
+                            </div>
+                            <div>
+                                Diabetes<input id='diabetes' name='familyHistory' type='checkbox' checked={ familyHistory.diabetes } onChange={ this.handleCheckboxChanges } />
+                            </div>
+                        </div>
+
+                        <div className='family'>
+                            Do you have any living, and how many? (check all that apply):
+                            <div>
+                                Siblings<input id='siblings' name='family' type='checkbox' checked={ family.siblings } onChange={ this.handleCheckboxChanges } />
+                                { renderFamilySelect(family.siblings , family.siblingCount,'siblingCount') }
+                            </div>
+                            <div>
+                                Parents<input id='parents' name='family' type='checkbox' checked={ family.parents } onChange={ this.handleCheckboxChanges } />
+                                { renderFamilySelect(family.parents, family.parentCount, 'parentCount') }
+                            </div>
+                            <div>
+                                Grandparents<input id='gParents' name='family' type='checkbox' checked={ family.gParents } onChange={ this.handleCheckboxChanges } />
+                                { renderFamilySelect(family.gParents, family.gParentCount, 'gParentCount') }
+                            </div>
+                        </div>
+
+                        <div className='education'>
+                            Check all educational credentials you have received:
+                            <div>
+                                High school diploma or GED equivalent<input id='diploma' name='education' type='checkbox' checked={ education.diploma } onChange={ this.handleCheckboxChanges } />
+                            </div>
+                            <div>
+                                Associate's Degree<input id='associates' name='education' type='checkbox' checked={ education.associates } onChange={ this.handleCheckboxChanges } />
+                            </div>
+                            <div>
+                                Bachelor's Degree<input id='bachelors' name='education' type='checkbox' checked={ education.bachelors } onChange={ this.handleCheckboxChanges } />
+                            </div>
+                            <div>
+                                Master's Degree<input id='masters' name='education' type='checkbox' checked={ education.masters } onChange={ this.handleCheckboxChanges } />
+                            </div>
+                            <div>
+                                PhD<input id='phd' name='education' type='checkbox' checked={ education.phd } onChange={ this.handleCheckboxChanges } />
+                            </div>
+                            <div>
+                                Other<input id='other' name='education' type='text' checked={ education.other } onChange={ this.handleTextChanges } />
+                            </div>
                         </div>
     
                         <div className='reason'>
-                            Why do you want to be a Mars explorer? <input className='reason' type='text' placeholder='Reason to explore...' name='reason' value={ reason } id='reason'></input>
+                            Why do you want to be a Mars explorer? <textarea onChange={ this.handleTextChanges } className='reason' type='text' placeholder='Reason to explore...' name='reason' value={ reason }></textarea>
                         </div>
                       <div className='button'>
                       </div>
                       </form>
                       { renderConfirmation() }
-                      <button onClick={ this.handleSubmit } type='button'>Submit</button>
+                      <button onClick={ this.handleSubmit } id='confirm' type='button'>Submit</button>
                     </div>
                 </React.Fragment>   
             )
--- forms-1/src/thankyou.js
+++
@@ -1,6 +0,0 @@
-import React, { Component } from 'react';
-
-class Thanks extends Component { 
-    constructor()
-
-}
